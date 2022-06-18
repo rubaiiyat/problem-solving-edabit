1578a077d764e2b93a095313f49c2c4b6e954acb
rubaiiyat <abirr1516@gmail.com>
2022-06-19 00:35:01 +0600

    Return the First Element in an Array

---
+++ 07.problem.js
@@ -0,0 +1,8 @@
+// Return the First Element in an Array
+
+function getFirstValue(arr) {
+  const totalValue = arr[0];
+  return totalValue;
+}
+const value = getFirstValue([-500, 0, 50]);
+console.log(value);
