42ad7afccffc38127aa82286111ab38b4fdbf70b
rubaiiyat <abirr1516@gmail.com>
2022-06-22 04:29:09 +0600

    Basic Variable Assignment

---
+++ 12.problem.js
@@ -0,0 +1,8 @@
+// Basic Variable Assignment
+function nameString(name) {
+  var b = "Edabit";
+  var result = name + b;
+  return result;
+}
+const result = nameString("Abir");
+console.log(result);
