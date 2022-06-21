2a77d56cd121d8014ec07079735fdd73d9638383
rubaiiyat <abirr1516@gmail.com>
2022-06-22 04:58:55 +0600

    Is the Number Less than or Equal to Zero?

---
+++ 15.problem.js
@@ -0,0 +1,10 @@
+// Is the Number Less than or Equal to Zero?
+const lessThanOrEqualToZero = (num) => {
+  if (num <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+const result = lessThanOrEqualToZero(-2);
+console.log(result);
